625fafc57b4f6dd5051fe4496a0e95bb2e057c7e
Joel Sanwiradi <joel.sanwiradi@rabobank.nl>
2023-03-06 20:50:47 +0100

    initial commit

---
+++ src/main/java/com/kleineman85/topchallenge/TopChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.kleineman85.topchallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TopChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TopChallengeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kleineman85/topchallenge/TopChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kleineman85.topchallenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TopChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
